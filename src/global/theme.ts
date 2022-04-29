659062c9e173ecc115fecfb837bb7094e0eb724e
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-29 19:01:33 -0300

    chore: adding theme application

---
+++ src/global/theme.ts
@@ -0,0 +1,25 @@
+export default {
+  colors: {
+    primary: "#5636D3",
+    secondary: "#FF872C",
+    secondary_light: "rgba(255, 135, 44,  0.3)",
+
+    success: "#12a454",
+    success_light: "rgba(18,134,84,0.1)",
+
+    attention: "#e83f5b",
+    attention_light: "rgba(232,63,91,0.1)",
+
+    shape: "#FFFFFF",
+    title: "#363f5f",
+    text: "#969cb2",
+    text_dark: "#000000",
+    background: "#F0F2F5",
+  },
+
+  fonts: {
+    regular: "",
+    medium: "",
+    bold: "",
+  },
+};
