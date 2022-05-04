05facaee3b1383a6618b70fe3c481cc64f1acbb7
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-05-04 08:57:10 -0300

    add: loading screen with SVG

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import logoDark from "../../assets/icons/logoTextLight.png";
-import { Container, Logo } from "./styles";
+import { CircleBack, CircleFront, Container, Logo } from "./styles";
 
 export function Loading() {
   return (
     <Container>
-      <Logo source={logoDark} />
+      <CircleBack>
+        <CircleFront>
+          <Logo />
+        </CircleFront>
+      </CircleBack>
     </Container>
   );
 }
--- src/pages/Loading/styles.ts
+++ src/pages/Loading/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components/native";
 
+import LogoLightSVG from "../../assets/icons/logoTextLight.svg";
+import LogoDarkSVG from "../../assets/icons/logoTextLight.svg";
+import { RFValue } from "react-native-responsive-fontsize";
+
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
@@ -8,4 +12,31 @@ export const Container = styled.View`
   background-color: ${({theme}) => theme.colors.background};
 `;
 
-export const Logo = styled.Image``;
+export const CircleBack = styled.View`
+  position: relative;
+  align-items: center;
+  justify-content: center;
+
+  width: ${RFValue(190)}px;
+  height: ${RFValue(190)}px;
+  border-radius: 1000px;
+
+  background-color: ${({ theme }) => theme.colors.primary_light};
+`;
+
+export const CircleFront = styled.View`
+  position: relative;
+  align-items: flex-end;
+  justify-content: center;
+
+  width: ${RFValue(148)}px;
+  height: ${RFValue(148)}px;
+  border-radius: 1000px;
+
+  background-color: ${({ theme }) => theme.colors.primary_light};
+`;
+
+export const Logo = styled(LogoLightSVG).attrs({
+  width: RFValue(128),
+  height: RFValue(107),
+})``;
