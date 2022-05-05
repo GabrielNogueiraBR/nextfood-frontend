5334025ee3cb89b6917ca1e65c8c74480bae28dc
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-05-04 21:57:26 -0300

    add: creating new react component (Login)

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+export function Login() {
+  return (
+    <Container>
+      <Text>{"Login"}</Text>
+    </Container>
+  );
+}
